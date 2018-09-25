ab87f65d4148149a0f9cf256e9d7108c0cb43bc6
flyq <flyq951@gmail.com>
2018-09-24 19:39:29 -0700

    fix transferFrom of ERC721

--- contracts/0913/Gateway_notary.sol
+++ contracts/0913/Gateway_notary.sol
@@ -32,6 +32,7 @@ contract Gateway_notary is Owned {
         public
     {
         require(erc20Address.isContract());
+
         ERC20 token = ERC20(erc20Address);
         if(token.transferFrom(msg.sender, address(this), amount)) {
             depositOfERC20[msg.sender][erc20Address] = depositOfERC20[msg.sender][erc20Address].add(amount);
@@ -44,6 +45,7 @@ contract Gateway_notary is Owned {
         public
     {
         require(erc721Address.isContract());
+
         ERC721 token = ERC721(erc721Address);
         token.transferFrom(msg.sender, address(this), tokenId);
         depositOfERC721[msg.sender][erc721Address].push(tokenId);
@@ -57,8 +59,9 @@ contract Gateway_notary is Owned {
         onlyAdmins
     {
         require(erc20Address.isContract());
-        ERC20 token = ERC20(erc20Address);
         require(!isTraded[txhash]);
+        
+        ERC20 token = ERC20(erc20Address);
         if(token.transfer(to, amount)) {
             withdrawOfERC20[to][erc20Address] = withdrawOfERC20[msg.sender][erc20Address].add(amount);
             isTraded[txhash] = true;
@@ -73,6 +76,7 @@ contract Gateway_notary is Owned {
     {
         require(erc721Address.isContract());    
         require(!isTraded[txhash]);
+        
         ERC721 token = ERC721(erc721Address);
         token.transferFrom(address(this), to, tokenId);
 
@@ -122,5 +126,5 @@ interface  ERC20 {
 }
 
 interface ERC721 {
-    function transferFrom(address from, address to, uint256 tokens) external returns (bool success);
+    function transferFrom(address from, address to, uint256 tokens) external;
 }
